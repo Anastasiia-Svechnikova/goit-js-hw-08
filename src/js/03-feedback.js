744499dd723eae1a0880da3c5a7fc5740d13940d
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-08-08 16:38:39 +0300

    task 3 completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,32 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const submitBtn = form.lastElementChild;
-console.log(5);
+
+const restoredData = JSON.parse(localStorage.getItem('feedback-form-state'));
+fillForm();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+const data = {};
+
+function onFormInput(e) {
+  data.email = form.elements.email.value;
+  data.message = form.elements.message.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(data);
+  e.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function fillForm() {
+  if (restoredData) {
+    form.elements.email.value = restoredData.email;
+    form.elements.message.value = restoredData.message;
+  }
+}
